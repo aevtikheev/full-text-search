5ca5bd82c834126ba75d5c1e8a4b8db2f5354ace
aevtikheev <eft000@gmail.com>
2021-04-01 23:25:02 +0300

    Add GIN indexes for the variety, winery and description fields

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.indexes import GinIndex
 from django.db import models
 
 
@@ -8,6 +9,15 @@ class Wine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
     variety = models.CharField(max_length=255)
     winery = models.CharField(max_length=255)
+
+    class Meta:
+        indexes = [
+            GinIndex(
+                name='desc_var_win_gin_idx',
+                fields=['description', 'variety', 'winery'],
+                opclasses=['gin_trgm_ops', 'gin_trgm_ops', 'gin_trgm_ops'],
+            ),
+        ]
 
     def __str__(self):
         return f'{self.id}'
